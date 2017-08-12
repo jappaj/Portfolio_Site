583a2dacdd03f130012b9e2c1721d7f66a8de853
jappaj <jd13@umbc.edu>
2017-08-11 21:55:35 -0400

    fix header and content for mobile, final mobile optimizations except for deviceOrientation

--- MVP_todo.md
+++ MVP_todo.md
@@ -1,8 +1,11 @@
 ## MVP To Do:
 * add single pages for each individual project
 * add full-resolution image on click on project pages
+* use slick native lazy loading
+
+* fix mobile version
+
 * add hyperlinks between pages
-* add nav to the header
 * update all css/js links to use build files
 * add mustache to the build pipeline
-* fix mobile version
+* header partial should have html head
--- src/js/template.js
+++ src/js/template.js
@@ -9,70 +9,46 @@
 // REMEMBER: remove jQuery wrapper when finished with development
 
 $(function() {
-    var regRed = $( "#registration-red" );
-    var regGreen = $( "#registration-green" );
-    var regBlue = $( "#registration-blue" );
-    var regNwCyan = $( "#registration-nw-cyan" );
-    var regNwMagenta = $( "#registration-nw-magenta" );
-    var regNwYellow = $( "#registration-nw-yellow" );
-    var regSeCyan = $( "#registration-se-cyan" );
-    var regSeMagenta = $( "#registration-se-magenta" );
-    var regSeYellow = $( "#registration-se-yellow" );
-    var nwReg = $( ".nw-registration" );
-    var seReg = $( ".se-registration" );
-    
     //fade function, @fcalderan on stackoverflow
     var divs = $('.header-fade-text'),
     limit = 20; /* scrolltop value when opacity should be 0 */
 
-$(window).on('scroll', function() {
-   var st = $(this).scrollTop();
+    $(window).on('scroll', function() {
+       var st = $(this).scrollTop();
 
-   /* avoid unnecessary call to jQuery function */
-   if (st <= limit) {
-      divs.css({ 'opacity' : (1 - st/limit) });
-   }
-});
-    //end fade function
+       /* avoid unnecessary call to jQuery function */
+       if (st <= limit) {
+          divs.css({ 'opacity' : (1 - st/limit) });
+       }
+    });
+        //end fade function
+
+    $('.image-container')/*.not('.multi-container')*/.slick({
+        fade: true,
+        adaptiveHeight: true,
+        arrows: true,
+        dots: true,
+        focusOnSelect: true,
+        swipeToSlide: true,
+        zIndex: 1
+
+    });
     
-$('.image-container').slick({
-    fade: true,
-    adaptiveHeight: true,
-    arrows: true,
-    dots: true,
-    focusOnSelect: true,
-    swipeToSlide: true,
-    zIndex: 1
-    
-});
-    
-    /* applyRegistrationEffect([regNwCyan, regNwMagenta, regNwYellow], function(nwReg) {
-        var nwRegPosX = nwReg.offset().left;
-        var nwRegWidth = nwReg.width();
-
-        return nwRegPosX + (nwRegWidth / 2);
-    }.bind(this, nwReg), function(nwReg) {
-        var nwRegPosY = nwReg.offset().top;
-        var nwRegHeight = nwReg.height();
-
-        return nwRegPosY + (nwRegHeight / 2);
-    }.bind(this, nwReg), 12, 12, (1 / 4), true);
-
-    applyRegistrationEffect([regSeCyan, regSeMagenta, regSeYellow], function(seReg) {
-        var seRegPosX = seReg.offset().left;
-        var seRegWidth = seReg.width();
-
-        return seRegPosX + (seRegWidth / 2);
-    }.bind(this, seReg), function(seReg) {
-        var seRegPosY = seReg.offset().top;
-        var seRegHeight = seReg.height();
-
-        return seRegPosY + (seRegHeight / 2);
-    }.bind(this, seReg), 12, 12, (1 / 4), true);  */
+    /*$('.multi-container').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        
+        fade: true,
+        arrows: true,
+        dots: true,
+        focusOnSelect: true,
+        swipeToSlide: true,
+        zIndex: 1
+    });*/
     
     $( ".nw-registration, .se-registration").each(function() {
         $(this).each(function() {
-            console.log($(this).children("img"));
             applyRegistrationEffect(
                 $(this).children("img"), function() {
                     var nwRegPosX = $(this).offset().left;
